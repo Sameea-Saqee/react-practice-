34097d0cb913df758cabe6b12d6fbea1c189e5df
Sameea-Student <144456655+Sameea-Student@users.noreply.github.com>
2024-12-01 12:13:19 +0500

    added fragment

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,10 +1,12 @@
 import FragmentComponent from "./components/FragmentComponent";
 import Message from "./components/Message";
+import Table from "./components/Table";
 function App() {
   return (
     <div className="App">
-      <Message />
-      <FragmentComponent /> 
+      <Message /> 
+      <FragmentComponent />
+      <Table></Table>
     </div>
   );
 }
---
+++ reactproject/src/components/Column.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Column() {
+  return (
+    <>
+      <td>Hello</td>
+      <td>There</td>
+    </>
+  );
+}
