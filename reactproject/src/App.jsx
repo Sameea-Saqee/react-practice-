f6daf8d23a9f72b067216c2e52183efbaf5e2429
Sameea-Student <144456655+Sameea-Student@users.noreply.github.com>
2024-12-01 17:09:27 +0500

    creating portals

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -2,6 +2,7 @@ import FragmentComponent from "./components/FragmentComponent";
 import Message from "./components/Message";
 import Table from "./components/Table";
 import Parent from "./components/Parent";
+import Modal from "./components/Modal";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <FragmentComponent />
       <Table></Table>
       <Parent />
+      <Modal />
     </div>
   );
 }
