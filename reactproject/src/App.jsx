0617c3f8cb554ce1356ba4cc40d3969703f9ded0
Sameea-Student <144456655+Sameea-Student@users.noreply.github.com>
2024-12-01 11:52:49 +0500

    add fragment component

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,8 +1,10 @@
+import FragmentComponent from "./components/FragmentComponent";
 import Message from "./components/Message";
 function App() {
   return (
     <div className="App">
       <Message />
+      <FragmentComponent /> 
     </div>
   );
 }
